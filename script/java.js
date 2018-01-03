69458f896711b9f8e5adb78e0879d1a7613e587b
priyangaprakash <33112410+priyangaprakash@users.noreply.github.com>
2018-01-03 11:53:11 +0530

    Create java.js

---
+++ script/java.js
@@ -0,0 +1,15 @@
+var a = prompt("what is your age??");
+switch(a)
+{
+case 18:
+alert("no right to vote");
+break;
+case 60:
+alert("right to vote");
+break;
+default:
+alert("default case");
+break;
+}
+confirm("Coming to the end");
+alert("OK, byeeeee!!");
